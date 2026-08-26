da88bea4949b3e420432fe03304c74ec72dcb583
Anton Shcherbakov <1mailanton@gmail.com>
2026-08-26 07:53:53 +0200

    write new styles to buyProductMenu.module.css and add forgotten file to "bind each product buttons with theirs pages" commit

--- src/components/UI/buyProductMenu/buyProductMenu.jsx
+++ src/components/UI/buyProductMenu/buyProductMenu.jsx
@@ -1,14 +1,34 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import cl from './buyProductMenu.module.css'
 import CoffeeImg from '../../../Images/coffee.png'
 import CondimentsList from "./condimentsList/condimentsList";
+import Context from "../../../index";
+import Beverage from '../../data/beverage'
+import {useLocation} from "react-router-dom";
 
 const BuyProductMenu = () => {
+    const {switchedBeverage} = useContext(Context)
     const [switchedCondiments, setSwitchedCondiments] = useState([]);
+    const [beverage, setBeverage] = useState(switchedBeverage || new Beverage);
+    const location = useLocation();
+
+    useEffect(() => {
+        setBeverage(switchedBeverage || new Beverage);
+    }, [location])
+    useMemo(() => {
+        console.log('change condiments list')
+    }, [switchedCondiments]);
     return (
         <div className={cl.productZone}>
-            <img className={cl.coffeeImg} src={CoffeeImg} />
-            <CondimentsList switchedCondiments={switchedCondiments}/>
+            <h1>{}</h1>
+            <div className={cl.cupAndHisName}>
+                <img className={cl.coffeeImg} src={CoffeeImg}/>
+                <h1 className={cl.beverageNameText}>{beverage.getDescription()}</h1>
+            </div>
+            <CondimentsList
+                switchedCondiments={switchedCondiments}
+                setSwitchedCondiments={setSwitchedCondiments}
+            />
         </div>
     );
 };
